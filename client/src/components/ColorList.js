043669f6aee8bad3813011cfad571aed931d3b30
dbriksza <dbriksza@gmail.com>
2019-10-18 13:03:19 -0400

    everything is working properly

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -7,7 +7,6 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -18,13 +17,22 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res =>
+        updateColors([
+          ...colors.filter(el => el.id != colorToEdit.id),
+          res.data
+        ])
+      )
+      .catch(err => console.log("Something went wrong: ", err));
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`/colors/${color.id}`)
+      .then(res => updateColors([...colors.filter(el => el.id != res.data)]))
+      .catch(err => console.log("Something went wrong: ", err));
   };
 
   return (
